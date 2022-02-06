2fc5761f8d122023603ad4e2c839d6b520476548
Alexandre P <@paquienalexandre0@gmail.com>
2022-02-06 23:02:49 +0100

    file init (moved from factories folder)

---
+++ scripts/templates/photographer.js
@@ -0,0 +1,26 @@
+function photographerTemplate (newPhotographer) {
+  const { _id, _name, _tagline, _price, location, pictureUrl } = newPhotographer;
+
+  const cardTemplate =
+  `
+  <article>
+    <a href="photographer.html?&id=${_id}">
+      <img src="${pictureUrl}" title="${_name}" alt="">
+      <h2>
+        ${_name}
+      </h2>
+    </a>
+    <p>
+      ${location}
+    </p>
+    <p>
+      ${_tagline}
+    </p>
+    <p>
+      ${_price}€/jour
+    </p>
+  </article>
+  `;
+  
+  return cardTemplate;
+}
